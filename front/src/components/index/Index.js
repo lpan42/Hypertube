7955f4c5cd166414f4614df82686cfbceb21272e
c-cchi <cchi@student.42.fr>
2020-05-28 22:50:34 +0200

    add fetch yts

--- front/src/components/index/Index.js
+++ front/src/components/index/Index.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import UserContext from '../../contexts/user/userContext';
 import setAuthToken from '../../utils/setAuthToken';
 import { useHistory } from "react-router-dom";
+import Search from './Search';
 
 const Index = () => {
     const userContext = useContext(UserContext);
@@ -13,9 +14,17 @@ const Index = () => {
         loadUser();
         //eslint-disable-next-line
       }, []);
+    // useEffect(() => {
+    //     loadUser();
+    //     if(!token && !user){
+    //        history.push('/register');
+    //     }
+    //     //eslint-disable-next-line
+    //   }, []);
 
     return (
         <div className="container">
+            <Search/>
             index
         </div>
     )
