fa66e030515a2201d25ff098509ae8d9f9c54e55
c-cchi <cchi@student.42.fr>
2020-06-04 19:10:06 +0200

    fetch 20 popular films

--- front/src/components/index/Search.js
+++ front/src/components/index/Search.js
@@ -1,53 +1,78 @@
-import React, { Fragment, useContext, useEffect, useState } from 'react'
+import React, { Fragment, useContext, useEffect, useState , useMemo} from 'react'
 import axios from 'axios';
 import Spinner from '../layout/Spinner';
-import CardMedia from '@material-ui/core/CardMedia';
+import {moviedbAPI_KEY} from '../moviedbAPI_KEY';
 
 const Search = () => {
     const [isFetching, setIsFetching] = useState(true);
     const [isError, setIsError] = useState(false);
     const [filmData, setfilmData] = useState({films: []});
+    const [searchInput, setSearchInput] = useState('movie');
 
     useEffect(() => {
-        getMovieList();
-    }, [])
-
-    const displayMovieList = () => {
-        for (let i = 0; i < filmData.films.length; i++){
-            console.log(i);
-            console.log(filmData.films[i].medium_cover_image);
+        function getFetchUrl(){
+            return 'https://api.themoviedb.org/3/discover/' + searchInput +'?api_key=' + moviedbAPI_KEY;
         }
-    }
 
-    const getMovieList = async() => {
-        try {
-            const filmList = await axios.get('https://yts.mx/api/v2/list_movies.json&sort_by=rating');
-            const films = filmList.data.data.movies;
-            setfilmData({
-                ...filmData,
-                films
-            });
-            displayMovieList();
-        }catch (error){
-            setIsError(true);
+        async function getData(){
+            const result = await axios(getFetchUrl())
+            .then(res => {
+                if (res.status === 200){
+                    const Data = res.data.results;
+                    setfilmData({
+                        ...filmData,
+                        films: Data
+                    })
+                    setIsFetching(false);
+                }else{
+                    setIsError(true);
+                    setIsFetching(false);
+                }
+            })
+            .catch ((err) => {
+                setIsError(true);
+                setIsFetching(false);
+            })
+            // setfilmData(result.data.results);
         }
-        setIsFetching(false);
-    }
+
+        getData();
+    }, [searchInput])
+
+    const filmList = useMemo(() => {
+        return filmData.films.map((film, key) => (
+                <div className="card" key={key}>
+                    <div className="image">
+                        <img src={'https://image.tmdb.org/t/p/w600_and_h900_bestv2/' + film.poster_path}/>
+                    </div>
+                    <div className="content">
+                        <div className="ui medium header">{film.original_title}</div>
+                        <div className="meta">
+                            <span className="date">{film.release_date}</span>
+                        </div>
+                    </div>
+                    <div className="extra">
+                    Rating:
+                        {Math.round(film.vote_average / 2)}
+                    </div>
+                </div>
+            ))
+    }, [filmData])
+
+    if (isFetching) return <Spinner/>
 
     return (
         <Fragment>
-            {isFetching === true ? 
-            <Spinner/> : 
-            <CardMedia
-                component="img"
-                alt=""
-                height="140"
-                image={filmData.films[0].medium_cover_image}
-            >
-            </CardMedia>
+        {
+            <div className='ui four doubling stackable cards'>
+            {filmList}
+            </div>
         }
         </Fragment>
     )
 }
 
 export default Search;
+
+//poster path https://image.tmdb.org/t/p/w600_and_h900_bestv2/+path
+//poster_path
