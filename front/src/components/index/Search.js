c107291613cbf7eb56a6f3e06582d114daf49682
c-cchi <cchi@student.42.fr>
2020-06-08 12:20:55 +0200

    add language, modify ui

--- front/src/components/index/Search.js
+++ front/src/components/index/Search.js
@@ -3,23 +3,33 @@ import React, { Fragment, useContext, useEffect, useState , useMemo} from 'react
 import axios from 'axios';
 import Spinner from '../layout/Spinner';
 import {moviedbAPI_KEY} from '../moviedbAPI_KEY';
+<<<<<<< HEAD
 =======
 import React, { Fragment, useContext, useEffect, useState } from 'react'
 import axios from 'axios';
 import Spinner from '../layout/Spinner';
 import CardMedia from '@material-ui/core/CardMedia';
 >>>>>>> add fetch yts
+=======
+import TrimInputStr from '../../utils/TrimInputStr'
+>>>>>>> add language, modify ui
 
 const Search = () => {
     const [isFetching, setIsFetching] = useState(true);
     const [isError, setIsError] = useState(false);
+    const [language, setLanguage] = useState('en');
     const [filmData, setfilmData] = useState({films: []});
 <<<<<<< HEAD
+<<<<<<< HEAD
     const [searchInput, setSearchInput] = useState('movie');
+=======
+    const [searchInput, setSearchInput] = useState('discover/movie/?api_key=' + moviedbAPI_KEY + '&language=' + language);
+    const [mouseHover, setMouseHover] = useState({mouseOn: false, filmKey: null});
+>>>>>>> add language, modify ui
 
     useEffect(() => {
         function getFetchUrl(){
-            return 'https://api.themoviedb.org/3/discover/' + searchInput +'?api_key=' + moviedbAPI_KEY;
+            return 'https://api.themoviedb.org/3/' + searchInput;
         }
 
         async function getData(){
@@ -41,17 +51,20 @@ const Search = () => {
                 setIsError(true);
                 setIsFetching(false);
             })
-            // setfilmData(result.data.results);
         }
 
         getData();
     }, [searchInput])
 
+
     const filmList = useMemo(() => {
         return filmData.films.map((film, key) => (
-                <div className="card" key={key}>
-                    <div className="image">
-                        <img src={'https://image.tmdb.org/t/p/w600_and_h900_bestv2/' + film.poster_path}/>
+                <div className="card" key={key} style={{boxShadow: 'none'}}>
+                    <div className="ui slide masked reveal image" style={{backgroundColor: 'black'}}>
+                        <img src={'https://image.tmdb.org/t/p/w600_and_h900_bestv2' + film.poster_path} className="visible content"/>
+                        <div className="hidden content center aligned" style={{backgroundColor: 'rgba(0,0,0,0.5)'}}>
+                            <span style={{padding: '2' ,fontFamily: 'Montserrat, sans-serif', fontSize: 16}}>{film.overview.split(' ').slice(0, 30).join(' ') + ' ......'}</span><br/>
+                        </div>
                     </div>
                     <div className="content">
                         <div className="ui medium header">{film.original_title}</div>
@@ -60,19 +73,34 @@ const Search = () => {
                         </div>
                     </div>
                     <div className="extra">
-                    Rating:
-                        {Math.round(film.vote_average / 2)}
+                        Rating:{film.vote_average}
                     </div>
                 </div>
             ))
     }, [filmData])
 
+    const setQuery = (filmSearch) => {
+        const filmSearchTrimmed = TrimInputStr(filmSearch);
+        console.log(filmSearchTrimmed)
+        if (filmSearchTrimmed == '' || !filmSearchTrimmed.replace(/\s/g, '').length){
+            return ('discover/movie/?api_key=' + moviedbAPI_KEY)
+        }
+        return 'search/movie/?api_key=' + moviedbAPI_KEY + '&query=' + filmSearchTrimmed;
+    }
+
     if (isFetching) return <Spinner/>
 
     return (
         <Fragment>
+            <div className="ui search">
+                <div className="ui icon input"></div>
+                <input className="prompt" type="text" placeholder="Search..." onChange={e => setSearchInput(setQuery(e.target.value))}/>
+                <i className="search icon"></i>
+            </div>
+            <div className="ui divider" style={{margin: '3em'}}></div>
+            <div class="ui centered grid">
         {
-            <div className='ui four doubling stackable cards'>
+            <div className='ui six doubling stackable cards'>
             {filmList}
             </div>
 =======
@@ -116,10 +144,12 @@ const Search = () => {
             </CardMedia>
 >>>>>>> add fetch yts
         }
+            </div>
         </Fragment>
     )
 }
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 export default Search;
 
@@ -128,3 +158,8 @@ export default Search;
 =======
 export default Search;
 >>>>>>> add fetch yts
+=======
+export default Search;
+
+//language --- &language=en-US after key
+>>>>>>> add language, modify ui
